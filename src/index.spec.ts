127e612e77ebc630ded95b4bf2a1e389c61a5979
Judah Anthony <judahtanthony@gmail.com>
2018-05-22 18:25:41 -0400

    Add a test for a really large file.  This seems to break.  TODO: fix test

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -27,13 +27,30 @@ const getWritable = cb => {
     }
   });
 };
+const getRandomReadable = left => {
+  return new Readable({
+    read: function(size) {
+      let len = Math.min(left, size);
+      let buff = Buffer.allocUnsafe(len);
+      for (let i = 0; i < len; ++i) {
+        buff.writeUInt8(Math.floor(Math.random() * 256), i);
+      }
+      this.push(buff);
+      left -= len;
+      console.log('Sending ('+len+'/'+left+')');
+      if (left <= 0) {
+        this.push(null);
+      }
+    }
+  });
+};
 
 const KNOWN_TEST_FILE = 'QUVTAgAAGENSRUFURURfQlkAYWVzY3J5cHQgMy4wNQCAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAEIz7OO6zNM6K3hu7TWh/aXzkxPhn/cyCvp+dzNsZilUAxaUWnX4R8CfM0rYXPBOXQ+S0rUf4kAp0us9MEPlNOWSZDgY3tP2/Y2oAm9rOcaWUyWySwDM01UxaDWcLOrp+w0YcH4isUGd2KVIHmmHl68FKwcyY2kikIL+dolT07q6Lo+wL8hVDAmxWNm2Oj23eAk=';
 const KNOWN_TEST_PASSWORD = 'test';
 const KNOWN_CONTENTS = 'test\n';
 
 describe('Encrypt', function() {
-  it ('should create an AES encrypted file', function(done) {
+  it('should create an AES encrypted file', function(done) {
     let s = getReadable(KNOWN_CONTENTS);
     let w = getWritable(contents => {
       assert.equal(contents.slice(0, 3).toString(), 'AES');
@@ -46,7 +63,7 @@ describe('Encrypt', function() {
 });
 
 describe('Decrypt', function() {
-  it ('should be able to decrypt an AES encrypted file', function(done) {
+  it('should be able to decrypt an AES encrypted file', function(done) {
     let s = getReadable(Buffer.from(KNOWN_TEST_FILE, 'base64'));
     let w = getWritable(contents => {
       assert.equal(contents.toString(), KNOWN_CONTENTS);
@@ -59,11 +76,35 @@ describe('Decrypt', function() {
 });
 
 describe('Encrypt-Decrypt', function() {
-  it ('should get the same contents after decrypting then before encrypting', function(done) {
+  it('should get the same contents after decrypting then before encrypting', function(done) {
     let s = getReadable(KNOWN_CONTENTS);
     let w = getWritable(contents => {
       assert.equal(contents.toString(), KNOWN_CONTENTS);
       done();
+    });
+    s.pipe(new Encrypt(KNOWN_TEST_PASSWORD))
+     .pipe(new Decrypt(KNOWN_TEST_PASSWORD))
+     .pipe(w)
+     .on('error', done);
+  });
+
+  it.skip('should be able to encrypt/decrypt large random files', function(done) {
+    this.timeout(10000);
+    let contentLength = 1000000;
+    let encryptedLength = 0;
+    let s = getRandomReadable(contentLength);
+    let w = new Writable({
+      write: function(chunk, encoding, callback) {
+        console.log('Receiving (' + chunk.length + ')');
+        encryptedLength += chunk.length;
+      },
+      final: function(callback) {
+        // I don't want to store everything, so let's just
+        // make sure the encryptedLength is greater than
+        // the contentLength.
+        assert.equal(contentLength < encryptedLength, true);
+        done();
+      },
     });
     s.pipe(new Encrypt(KNOWN_TEST_PASSWORD))
      .pipe(new Decrypt(KNOWN_TEST_PASSWORD))
